09112791c8f2b73e58aa0bca5955a4cf790e6ec9
thura <thuraaung2493@gmail.com>
2018-05-27 22:04:30 +0630

    readme update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(HouseFeaturesSeeder::class);
-        // $this->call(HouseTypesSeeder::class);
-        // $this->call(RegionsSeeder::class);
-        // $this->call(RoleSeeder::class);
-        // $this->call(AdminSeeder::class);
-        // $this->call(SuperAdminSeeder::class);
-        // $this->call(UserSeeder::class);
+        $this->call(HouseFeaturesSeeder::class);
+        $this->call(HouseTypesSeeder::class);
+        $this->call(RegionsSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(SuperAdminSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(HousesSeeder::class);
     }
 }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,11 @@
+Before you can test this project =>
+    1. DB Seed,
+    2. create two folders "storage/app/public/photos/" . profiles and
+        thumbnails,
+    3. create a symbolic link from public/storage to  storage/app/public
+        (php artisan storage:link).
+
+
 **** Models ****
 -> House ['houseDetail', 'location', 'user', 'houseType', 'galleries']
 -> HouseDetail
